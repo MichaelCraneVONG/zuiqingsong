2116db6a6a737285cddebb8d6929c5e9efa4cbe3
书呆子Crane <MichaelCraneVONG>
2016-03-27 15:48:29 +0800

    Initial Commit

---
+++ zuiqingsong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zuiqingsong
+//
+//  Created by myApple on 16/3/27.
+//  Copyright © 2016年 crane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zuiqingsong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zuiqingsong
+//
+//  Created by myApple on 16/3/27.
+//  Copyright © 2016年 crane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
